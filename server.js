d03d405a71ce7fb3021b84bcdbe8571b688c88bf
10XMairing <10xpumba@gmail.com>
2018-11-26 13:54:43 +0530

    test

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const http = require('http');
 const app =require('./app');
 
 const port = process.env.PORT || 3000;
+const testPort = 3000;
 
 const server = http.createServer(app);
 
 
-server.listen(port);
+server.listen(testPort);
 
